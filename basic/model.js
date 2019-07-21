66abb1834a3a82286ce95892819ac2280c9f55cb
KyleKant <onhanam1234@gmail.com>
2019-07-21 14:48:47 +0700

    commit to repository

--- basic/model.js
+++ basic/model.js
@@ -9,21 +9,42 @@ app.config(function ($routeProvider, $locationProvider) {
 		templateUrl: 'Primary.html',
 		controller: 'myControllerPrimary'
 	})
-	.when('/Social', {
-		templateUrl: 'Social.html'
+	.when('/social', {
+		templateUrl: 'Social.html',
+		controller: 'myControllerSocial'
 	})
-	.when('/Promotion', {
-		templateUrl: 'Promotion.html'
+	.when('/promotion', {
+		templateUrl: 'Promotion.html',
+		controller: 'myControllerPromotion'
 	})
 	.otherwise({ redirectTo: '/' });
-	$locationProvider.html5Mode(false);
+	$locationProvider.html5Mode(true);
 })
 
 app.controller('myControllerPrimary', function ($scope, $http) {
 	$http.get('http://localhost/angular_php/index.php/welcome/getdata')
 	.then(function(response){
 		console.log(response.data);
+		$scope.data = response.data;
 	}, function(response){
+		console.log("Get data failed!");
+	})
+})
+
+app.controller('myControllerSocial', function ($scope, $http) {
+	$http.get('data/dataSocial.json').then(
+		function(response){
+		$scope.data = response.data;
+	}, function(res){
 
 	})
+})
+
+app.controller('myControllerPromotion', function ($scope, $http) {
+	$http.get('data/dataPromotion.json').then(
+		function(response){
+			$scope.data = response.data;
+		}, function(response){
+
+		})	
 })
